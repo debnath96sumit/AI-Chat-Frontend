d6c145f50fa8838293407ab52adbefd27a19afe0
sumit <sumit.debnath@webskitters.in>
2025-08-04 20:09:44 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
